37ce9509bb3f2ed5ccf7e75fe33aaa47e3a76b20
atreder03-lang <atreder03@gmail.com>
2026-01-13 16:44:06 +0530

    Add Firebase configuration and initialization

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCpVYlhWxkGwvbO0NUCXeT9yNYuTYfkF2Q",
+    authDomain: "rajagame94-16274.firebaseapp.com",
+    projectId: "rajagame94-16274",
+    storageBucket: "rajagame94-16274.firebasestorage.app",
+    messagingSenderId: "842689918760",
+    appId: "1:842689918760:web:262f823e0fbe30ffcac21d"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
