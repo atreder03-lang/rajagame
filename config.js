1ab4dc0d6e351cbf908b9c775aec8d5c7df83224
atreder03-lang <atreder03@gmail.com>
2026-01-14 12:13:23 +0530

    Refactor Firebase initialization and export config

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 // IMPORT FIREBASE
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } 
-from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // YOUR FIREBASE CONFIG
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCpVYlhWxkGwvbO0NUCXeT9yNYuTYfkF2Q",
   authDomain: "rajagame94-16274.firebaseapp.com",
   projectId: "rajagame94-16274",
@@ -13,6 +12,5 @@ const firebaseConfig = {
   appId: "1:842689918760:web:262f823e0fbe30ffcac21d"
 };
 
-// INIT
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
